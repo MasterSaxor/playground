2f7c2f2801ea0cb4aed4343b458c29661f13a387
MasterSaxor <70054062+MasterSaxor@users.noreply.github.com>
2025-10-07 11:15:01 +0800

    Create dict_csv.py

---
+++ dict_csv.py
@@ -0,0 +1,10 @@
+'''
+Using the dictionary of following data, save the information to a csv file with the keys as the headers 
+and the values as the rows of data:
+
+data = {
+ 'name' : ['Dave', 'Dennis', 'Peter', 'Jess'],
+ 'language': ['Python', 'C', 'Java', 'Python']
+}
+
+'''
