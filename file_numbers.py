743ffe87ee8e8c115d3beb278a21270757ab4580
MasterSaxor <70054062+MasterSaxor@users.noreply.github.com>
2025-10-07 11:49:15 +0800

    Update and rename files.py to file_numbers.py

---
+++ file_numbers.py
@@ -0,0 +1,1 @@
+# Ask a user for a filename, and then write the numbers 1 to 10, one per line, to that file.
--- files.py
+++
@@ -1,8 +0,0 @@
-'''
-1) Open a file named “data.txt” in the current directory for reading.
-2) Open a file named “data.txt” in the directory above the current one for writing.
-3) Close the files in exercises 1 and 2.
-4) Given an open file “infile,” read and print each line in the file.
-5) Ask a user for a filename, and then write the numbers 1 to 10, one per line, to that file.
-6) Assume that you have a data file named “data.txt” that consists of integers, one per line. Find and print the average of those numbers. 
-'''
