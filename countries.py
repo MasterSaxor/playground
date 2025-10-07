6e5cd5dad5748b4cd9441a4d05a030d3418f3555
MasterSaxor <70054062+MasterSaxor@users.noreply.github.com>
2025-10-07 11:28:34 +0800

    Create countries.py

---
+++ countries.py
@@ -0,0 +1,16 @@
+'''
+Write a program that prompts the user to input a word and a filename. 
+The program should print each line of the file that contains the word.
+
+Here is an example run of the program:
+
+    Enter a word: United
+    Enter a filename: countries.csv
+    United Arab Emirates,9890402,83600,118
+    United Kingdom,67886011,241930,281
+    United States of America,331002651,9147420,36
+
+The user might incorrectly type the filename (Ex: countries.txt instead of countries.csv). 
+Your program should output an error message if the file is not found, 
+and keep prompting the user to input a filename until the file is found.
+'''
