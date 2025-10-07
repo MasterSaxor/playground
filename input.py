f335aaac371ae097f738a6e59f9024c23dd8bee8
MasterSaxor <70054062+MasterSaxor@users.noreply.github.com>
2025-10-07 11:00:54 +0800

    Create input.py

---
+++ input.py
@@ -0,0 +1,14 @@
+'''
+The file input.txt is shown. The file represents a set of integers. 
+Line 1 of the file specifies how many integers follow. 
+Write a program that reads from this file and determines the average of the numbers following the first line.
+
+input.txt
+
+    n: 5
+    25
+    13
+    4
+    6
+    19
+'''
