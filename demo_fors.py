cf08445759fcc5a5607e50ba50dba86281bca689
MasterSaxor <70054062+MasterSaxor@users.noreply.github.com>
2025-11-26 02:51:30 +0000

    last discussion it1104

--- demo_fors.py
+++ demo_fors.py
@@ -1,33 +1,30 @@
-'''
-password = input("What's the magic password? ").strip()
-
-while password != "eyy":
-    password = input("Password: ").strip()
+nums = [3, 5, 2, 1, 7, 8, 5]
 
 
-finished = False
+print(len(nums))
+print()
 
-while not finished:
-    password = input("Password: ").strip()
-    finished = password == "saxor"
+# Accessing values by item/name
+for num in nums:
+    print(num)
 
-    
-password = ""
-while password != "saxor":
-    password = input("Password: ").strip()
-'''
+print()
 
-targets = [2, 3, 5, 4, -1, 1]
-index = 0
+# Accessing values by index
+print(nums[0])
+print(nums[3])
+print()
 
-count = 0
-while targets[index] != -1:
-    count += 1
-    index = targets[index]
-    print(f"Loop No.{count}   Index:{index}")
+odd_no_index = []
+for index in range(len(nums)):
+    print(f"At the index {index}, the value is {nums[index]}")
 
+    if index % 2 == 1:
+        odd_no_index.append(nums[index])
 
-    
+print()
+print(odd_no_index)
+print()
 
-
-
+for i, value in enumerate(nums):
+    print(f"Item No.{i+1}, the value is {value}")
--- while.py
+++ while.py
@@ -1,19 +1,33 @@
 '''
-password = input("Password: ").strip()
-while password != "ey":
-    password = input("Password: ").strip()
+password = input("What's the magic password? ").strip()
 
-    
+while password != "saxor":
+    password = input("What's the magic password? ").strip()
+
+print("Welcome Master Saxor!")
+
+
 finished = False
 while not finished:
     password = input("Password: ").strip()
-    finished = password == "ey"
+
+    finished = password == "saxor"
+
+    
+password = ""
+
+while password != "saxor":
+    password = input("Password: ").strip()
 '''
 
 targets = [2, 3, 5, 4, -1, 1]
 index = 0
 count = 0
+
 while targets[index] != -1:
     index = targets[index]
     count += 1
-    print(f"Loopn No.{count}  Index: {index}")
+
+    print(f"Loop No.{count}, the index is {index}")
+
+    
